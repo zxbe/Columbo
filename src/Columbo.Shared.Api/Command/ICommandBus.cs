22bfc6893dad8d90a8836ea7e58fe0b94babdd34
Piotr Bielski <bpiotrrr@gmail.com>
2018-12-13 21:14:44 +0100

    created Columbo.Shared.Api project

---
+++ src/Columbo.Shared.Api/Command/ICommandBus.cs
@@ -0,0 +1,12 @@
+﻿using Columbo.Shared.Kernel.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Columbo.Shared.Api.Command
+{
+    public interface ICommandBus
+    {
+        void Send<TCommand>(TCommand command) where TCommand : ICommand;
+    }
+}
---
+++ src/Columbo.Shared.Api/Command/ICommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Columbo.Shared.Kernel.Command;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Columbo.Shared.Api.Command
+{
+    public interface ICommandHandler<in TCommand> where TCommand : ICommand
+    {
+        void Handle(TCommand command);
+    }
+}
