c5b49def9f45cced0d3be4790327583617483d63
Piotr Bielski <bpiotrrr@gmail.com>
2019-01-25 13:54:08 +0100

    started preparing interface in sts project

---
+++ src/Columbo.IdentityProvider.Sts/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Columbo.IdentityProvider.Sts.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string ReturnUrl { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
